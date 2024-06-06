2ec25d4df08efeb2f12615b65d2b82b8f69dff2b
Maticruces <104586656+Maticruces@users.noreply.github.com>
2024-06-06 16:39:39 -0400

    arreglo de main con metodos de busqueda
    aqui intente dejar lo mas explicado para el momento en que lo uses en en metodos de busqueda, ademas deje los 2 archivos .h juntos para que sirvan los 2

--- main.cpp
+++ main.cpp
@@ -53,25 +53,48 @@ int main(int argc, char** argv) {
     int n = atoi(argv[1]); // Almacenará la cantidad de elementos
 
     // Abrimos el archivo .csv en modo de escritura
-    std::ofstream file("resultados_20000.csv");
+    std::ofstream file("Resultados_String_1000.csv"); // Cambiar el nombre del archivo .csv aca cada que haces un archivo cambias el numero para que nos lo separe sino tambien lo arreglo despues.
+
     file << "Ejecucion;Metodo;Tiempo_Segundos\n"; // Escribimos la cabecera del archivo .csv
 
     // Realizamos las pruebas varias veces
     for (int test = 1; test <= 20; test++) {
 
-        HashAbierto hashTable(n); // Crearemos una tabla de hash con n elementos
+        HashAbiertoString hashTable(n); // Crearemos una tabla de hash con n elementos
 
         // Creamos n usuarios y los insertamos en la tabla hash
         auto start = std::chrono::high_resolution_clock::now();
         for(int i = 1; i <= n; i++) {
             Usuario Usuario = Usuarios[i];
-            hashTable.insert(Usuario.UsuarioID, Usuario);
+            hashTable.insert(Usuario.UsuarioName, Usuario);
         }
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> duration = end - start;
 
+/*
+        // Buscamos cada usuario en la tabla hash
+        start = std::chrono::high_resolution_clock::now();
+        for(int i = 1; i <= n; i++) {
+            Usuario Usuario = Usuarios[i];
+            hashTable.search(Usuario.UsuarioName, Usuario);  //buscamos el usuario (elemento) en la tabla hash
+        }
+        end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> Busqueda = end - start;
+
+        // Buscamos de usuario inecistentes en la tabla hash
+        start = std::chrono::high_resolution_clock::now();
+        for(int i = 1; i <= n; i++) {
+        	Usuario Usuario = Usuarios[i];
+            hashTable.search(-Usuario.UsuarioName, Usuario);
+        }
+        end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> Busqueda_f = end - start;
+*/
         // Escribimos los resultados en el archivo .csv
-        file << "HashAbiertoInt" << ";insert;" << duration.count() << "\n";
+        
+        file << "HashAbiertoInt" << ";" << n << ";insert;" << duration.count() << "\n";
+        //file << "HashAbiertoInt" << ";" << n << ";search;" << Busqueda.count() << "\n";  //escribimos el tiempo de busqueda
+        //file << "HashAbiertoInt" << ";" << n << ";search_nouser;" << Busqueda_f.count() << "\n"; //escribimos el tiempo de busqueda de un usuario inexistente
     }
 
     file.close(); // Cerramos el archivo .csv
